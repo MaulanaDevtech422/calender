c92ba71f08526a1019b4c747b0e6f8270d5720f9
MaulanaDevtech422 <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-03-18 16:00:13 +0700

    Create calender.sh

---
+++ calender.sh
@@ -0,0 +1,17 @@
+                                     Modified
+#ini adalah script bash untuk mengetahui calender dan waktu
+
+ps | lolcat
+date | lolcat
+cal | lolcat
+
+#cara install di terminal
+# apt update &&  apt upgrade -y
+# pkg install ruby
+# gem install lolcat
+# pkg install git
+# git https://github.com/MaulanaDevtech422/calender
+# ls
+# cd calender
+# chmod +x calender.sh
+# ./calender.sh
